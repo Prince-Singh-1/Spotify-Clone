f7a24032cf3f62f4ad076d186312776e945e62e0
Prince Singh <prince22657989@gmail.com>
2026-06-11 13:16:53 +0530

    Fix CORS for Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,13 @@ const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://spotify-clone-peach-tau.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 
 app.use(express.json()); // to parse req.body
