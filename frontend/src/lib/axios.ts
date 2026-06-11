73a525207a1c5c5effc7616f47488ed301d987dc
Prince Singh <prince22657989@gmail.com>
2026-06-11 13:11:45 +0530

    Fix API URL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 	baseURL:
-		import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+		import.meta.env.MODE === "development"
+			? "http://localhost:5000/api"
+			: import.meta.env.VITE_API_URL + "/api",
 });
